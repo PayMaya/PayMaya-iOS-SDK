b514f19ea244321150784553ebd1991bf1839c82
Elijah Joshua Cayabyab <ebcayabyab@voyagerinnovation.com>
2016-04-28 13:59:16 +0800

    Card list VC

--- Example/PayMayaSDK/PMDCardVaultViewController.m
+++ Example/PayMayaSDK/PMDCardVaultViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    UIBarButtonItem *addCardBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(didTapAddCardBarButtonItem:)];
+    self.navigationItem.rightBarButtonItem = addCardBarButtonItem;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +26,9 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)didTapAddCardBarButtonItem:(id)sender
+{
+    
 }
-*/
 
 @end
