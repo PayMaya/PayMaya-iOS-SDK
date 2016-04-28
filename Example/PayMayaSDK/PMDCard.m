4fa27da866ed16ab8ed021fb3c63fd0be3629654
Elijah Joshua Cayabyab <ebcayabyab@voyagerinnovation.com>
2016-04-29 05:27:49 +0800

    Add card model

---
+++ Example/PayMayaSDK/PMDCard.m
@@ -0,0 +1,13 @@
+//
+//  PMDCard.m
+//  PayMayaSDK
+//
+//  Created by Elijah Cayabyab on 29/04/2016.
+//  Copyright © 2016 PayMaya Philippines, Inc. All rights reserved.
+//
+
+#import "PMDCard.h"
+
+@implementation PMDCard
+
+@end
